80fa0097a3e3033e7b4a68c6b6c60646efafa54a
William Koller <williamkoller404@gmail.com>
2022-11-28 10:23:12 -0300

    refactor: add @nestjs/config and set configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get('MONGODB_URI'),
+      }),
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('Main');
-  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get<ConfigService>(ConfigService);
+  const port = config.get<number>('PORT');
+
   await app.listen(port, () => logger.log(`Server listen at port: ${port}`));
 }
 bootstrap();
